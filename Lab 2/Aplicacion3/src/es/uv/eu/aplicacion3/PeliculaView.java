e4d3596a79119683181a81679ffc05cf3b161370
InessSUS <inesjaso@icloud.com>
2025-10-26 15:41:18 +0100

    Probando repositorio

---
+++ Lab 2/Aplicacion3/src/es/uv/eu/aplicacion3/PeliculaView.java
@@ -0,0 +1,47 @@
+package es.uv.eu.aplicacion3;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Natalia Tauste
+ * @author Inés Jaso Pernod
+ */
+public class PeliculaView extends JFrame {
+    private PeliculaModel peliculas;
+    private ListaPanel listaPanel;
+    private PeliculaMenu peliculaMenu;
+    private JButton exit;
+
+    public PeliculaView(PeliculaModel peliculas) {
+        super("Aplication 1: Lista de peliculas");
+
+        this.peliculas = peliculas;
+        this.setLayout(new BorderLayout());
+
+        this.setSize(700, 400);
+
+        JLabel label = new JLabel("Lista de peliculas");
+        label.setFont(new Font("Sans", Font.BOLD, 14));
+
+        listaPanel = new ListaPanel(peliculas);
+        listaPanel.mostrar();
+
+        exit = new JButton("Salir");
+        exit.setActionCommand("buttonExit");
+
+        this.add(label, BorderLayout.NORTH);
+        this.add(listaPanel, BorderLayout.CENTER);
+        this.add(exit, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+    }
+    public void setActionListener(ActionListener actionListener){
+        exit.addActionListener(actionListener);
+    }
+}
---
+++ Lab 2/Aplicacion4/src/es/uv/eu/aplicacion4/ListaPanel.java
@@ -0,0 +1,36 @@
+package es.uv.eu.aplicacion4;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+/**
+ *
+ * @author Entornos de Usuario
+ */
+public class ListaPanel extends JPanel {
+    private PeliculaModel peliculas;
+    private JTextArea textArea;
+    private JScrollPane scroll;
+
+    public ListaPanel(PeliculaModel peliculas){
+        this.peliculas = peliculas;
+
+        setLayout(new BorderLayout());
+        textArea = new JTextArea(20, 60);
+        textArea.setEditable(false);
+        textArea.setBackground(Color.lightGray);
+        textArea.setFont(new Font("Sans", Font.BOLD, 12));
+        scroll = new JScrollPane(textArea);
+
+        this.add(scroll, BorderLayout.CENTER);
+        this.setVisible(true);
+    }
+
+    public void mostrar(){
+        textArea.append(peliculas.toString());
+    }
+}
